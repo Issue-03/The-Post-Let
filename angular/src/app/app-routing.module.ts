28a976d244b972a35ce21543126bf078f9278e37
Issue-03 <ishucuriosity@gmail.com>
2020-07-30 07:14:38 +0530

    added global stylesheets and scripts

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SearchComponent } from './search/search.component';
+import { FriendRequestComponent } from './friend-request/friend-request.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "feed", pathMatch: "full" },
+  { path: "register", component: RegisterComponent, canActivate: [AuthService] },
+  { path: "login", component: LoginComponent, canActivate: [AuthService] },
+  { path: "feed", component: FeedComponent, canActivate: [AuthService], data: { loggedIn: true } },
+  { path: "profile", component: ProfileComponent, canActivate: [AuthService], data: { loggedIn: true } },
+  { path: "messages", component: MessagesComponent, canActivate: [AuthService], data: { loggedIn: true } },
+  { path: "search", component: SearchComponent, canActivate: [AuthService], data: { loggedIn: true } },
+  { path: "requests", component: FriendRequestComponent, canActivate: [AuthService], data: { loggedIn: true } },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
